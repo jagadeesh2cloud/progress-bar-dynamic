9d55d0a28a6f0cc3e373abd5dc82358600ab551d
CTS\556064 <jagadeesh.v@cognizant.com>
2018-12-26 14:54:25 +0530

    updated changes for tablet view

--- src/app/stickyheader/stickyheader.component.ts
+++ src/app/stickyheader/stickyheader.component.ts
@@ -23,28 +23,30 @@ export class StickyheaderComponent implements OnInit {
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(e) {
+     let screenWidth = screen.width;
+     if (screenWidth < 992) { //TODO: Confirm the min screen width to support sticky header
+      let scrollValue = window.scrollY;
+      let stickyElement = document.querySelector('.subnav-toggle');
+      let subNavElement = document.querySelector('.subnav-toggle .navbar-toggler');
+      let subNavMenuItems = document.querySelector('#navbarToggleCollapse');
+      let subNavMenuItemsShowed = document.querySelector('#navbarToggleCollapse.show');
  
-     let scrollValue = window.scrollY;
-     let stickyElement = document.querySelector('.subnav-toggle');
-     let subNavElement = document.querySelector('.subnav-toggle .navbar-toggler');
-     let subNavMenuItems = document.querySelector('#navbarToggleCollapse');
-     let subNavMenuItemsShowed = document.querySelector('#navbarToggleCollapse.show');
-
-     if (scrollValue > 104) { // The value given here must be the height of all elements above the expected sticky nav
-         stickyElement.classList.add('affix')
-         subNavElement.classList.add('d-inline');
-         subNavElement.classList.remove('d-none');
-         subNavMenuItems.classList.remove('d-none');
-     } else {
-         stickyElement.classList.remove('affix')
-         subNavElement.classList.remove('d-inline');
-         subNavElement.classList.add('d-none');
-         subNavMenuItems.classList.add('d-none');
-         if(subNavMenuItemsShowed) {
-          subNavMenuItemsShowed.classList.remove('show');
-         }
+      // TODO: The below value could change in actual screen.
+      // The value given below must be the height of all elements above the expected sticky nav
+      if (scrollValue > 104) { 
+          stickyElement.classList.add('affix')
+          subNavElement.classList.add('d-inline');
+          subNavElement.classList.remove('d-none');
+          subNavMenuItems.classList.remove('d-none');
+      } else {
+          stickyElement.classList.remove('affix')
+          subNavElement.classList.remove('d-inline');
+          subNavElement.classList.add('d-none');
+          subNavMenuItems.classList.add('d-none');
+          if(subNavMenuItemsShowed) {
+           subNavMenuItemsShowed.classList.remove('show');
+          }
+      }
      }
-
   }
-
 }
