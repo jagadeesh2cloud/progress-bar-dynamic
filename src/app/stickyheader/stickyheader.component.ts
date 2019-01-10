c594067c7715720b11f892c1adbe942394dd621a
CTS\556064 <jagadeesh.v@cognizant.com>
2019-01-10 18:25:10 +0530

    changes to alter the sticky header based on window resize

--- src/app/stickyheader/stickyheader.component.ts
+++ src/app/stickyheader/stickyheader.component.ts
@@ -1,52 +1,50 @@
-import { Component, OnInit, HostListener, Inject } from '@angular/core';  
-import { trigger, state, transition, style, animate } from '@angular/animations';  
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-stickyheader',
   templateUrl: './stickyheader.component.html',
   styleUrls: ['./stickyheader.component.css'],
-  animations:[ 
+  animations: [
     trigger('fade',
-    [ 
-      state('void', style({ opacity : 0})),
-      transition(':enter',[ animate(300)]),
-      transition(':leave',[ animate(500)]),
-    ]
-)]
+      [
+        state('void', style({ opacity: 0 })),
+        transition(':enter', [animate(5000)]),
+        transition(':leave', [animate(10000)]),
+      ]
+    )]
 })
 export class StickyheaderComponent implements OnInit {
 
   constructor(@Inject(DOCUMENT) document) { }
 
-  ngOnInit() {  }
+  ngOnInit() { }
 
   @HostListener('window:scroll', ['$event'])
-  onWindowScroll(e) {
-     let screenWidth = screen.width;
-     if (screenWidth < 992) { //TODO: Confirm the min screen width to support sticky header
-      let scrollValue = window.scrollY;
-      let stickyElement = document.querySelector('.subnav-toggle');
-      let subNavElement = document.querySelector('.subnav-toggle .navbar-toggler');
-      let subNavMenuItems = document.querySelector('#navbarToggleCollapse');
-      let subNavMenuItemsShowed = document.querySelector('#navbarToggleCollapse.show');
- 
-      // TODO: The below value could change in actual screen.
-      // The value given below must be the height of all elements above the expected sticky nav
-      if (scrollValue > 104) { 
-          stickyElement.classList.add('affix')
-          subNavElement.classList.add('d-inline');
-          subNavElement.classList.remove('d-none');
-          subNavMenuItems.classList.remove('d-none');
-      } else {
-          stickyElement.classList.remove('affix')
-          subNavElement.classList.remove('d-inline');
-          subNavElement.classList.add('d-none');
-          subNavMenuItems.classList.add('d-none');
-          if(subNavMenuItemsShowed) {
-           subNavMenuItemsShowed.classList.remove('show');
-          }
+  onWindowScrollAndResize(e) {
+    let screenWidth = screen.width;
+    let scrollValue = window.scrollY;
+    let stickyElement = document.querySelector('.subnav-toggle');
+    let subNavElement = document.querySelector('.subnav-toggle .navbar-toggler');
+    let subNavMenuItems = document.querySelector('#navbarToggleCollapse');
+    let subNavMenuItemsShowed = document.querySelector('#navbarToggleCollapse.show');
+    //TODO: Confirm the min screen width to support sticky header
+    //TODO: The below value could change in actual screen.
+    //The value given below must be the height of all elements above the expected sticky nav
+    if (screenWidth < 992 && scrollValue > 104) {
+      stickyElement.classList.add('affix')
+      subNavElement.classList.add('d-inline');
+      subNavElement.classList.remove('d-none');
+      subNavMenuItems.classList.remove('d-none');
+    } else {
+      stickyElement.classList.remove('affix')
+      subNavElement.classList.remove('d-inline');
+      subNavElement.classList.add('d-none');
+      subNavMenuItems.classList.add('d-none');
+      if (subNavMenuItemsShowed) {
+        subNavMenuItemsShowed.classList.remove('show');
       }
-     }
+    }
   }
 }
